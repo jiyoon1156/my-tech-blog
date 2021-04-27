e0b569ccd362562b38efb0d7f4f74b27b8c8965b
jiyoonhur <jiyoon1156@gmail.com>
2021-04-27 21:47:54 +0900

    [CHORE] CRA with typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Tables from './tables'
+
+const App: React.FC = () => {
+  return (
+    <>
+      <div>My Tech Blog List</div>
+      <Tables />
+    </>
+  );
+};
+
+export default App;
