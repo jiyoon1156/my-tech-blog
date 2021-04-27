193491ee90055c1b3d9ea7ec8c8c97c6df82b393
jiyoonhur <jiyoon1156@gmail.com>
2021-04-27 21:48:54 +0900

    [CHORE] Parse REAME.md to json using Python3

---
+++ test.py
@@ -0,0 +1,33 @@
+import json
+from collections import OrderedDict
+
+file_data = OrderedDict()
+
+fp = open('README.test.md', 'r')
+lines = fp.readlines()
+
+data = []
+
+for line in lines:
+	if "|" in line:
+		a, idx, titleLink, date, e = line.split("|")
+		tmp = titleLink.split("(")
+		if len(tmp) > 1:
+			title, url = tmp[0], tmp[1]
+			title = title[1:-1]
+			url = url[:-1]
+
+			data.append({
+				"id": int(idx),
+				"title": title,
+				"summary": "",
+				"url": url,
+				"date": date,
+				"tags": [""]
+			})
+
+data = sorted(data, key=(lambda x:x['id']))
+
+file_data["data"] = data
+with open('./public/data.json', 'w', encoding="utf-8") as make_file:
+	json.dump(file_data, make_file, ensure_ascii=False, indent='\t')
