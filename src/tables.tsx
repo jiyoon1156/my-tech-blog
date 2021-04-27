c1d069acef0293f665ad6cbbb3f9fb26d128e3e8
jiyoonhur <jiyoon1156@gmail.com>
2021-04-27 23:33:50 +0900

    [FEAT]Deploy to github pages

--- src/tables.tsx
+++ src/tables.tsx
@@ -1,19 +1,6 @@
 import React from 'react';
 import { data } from './data.json'
 
-const people = [
-  {
-    name: 'Jane Cooper',
-    title: 'Regional Paradigm Technician',
-    department: 'Optimization',
-    role: 'Admin',
-    email: 'jane.cooper@example.com',
-    image:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  // More people...
-]
-
 const Tables: React.FC = () => {
 	console.log(data)
 	return (
